b73d31e625f9fc1be3c1119e7c9bd7040ea78a72
Flores <edwin.carl.b.flores@accenture.com>
2018-07-16 19:46:17 +0800

    Finished Thymeleaf

--- src/main/java/com/booking/controllers/ViewController.java
+++ src/main/java/com/booking/controllers/ViewController.java
@@ -1,13 +1,20 @@
 package com.booking.controllers;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
 	
 	@RequestMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("datetime", new Date());
+		model.addAttribute("username", "John Doe");
+		model.addAttribute("mode", "production");
+		
 		return "index";
 	}
 }
